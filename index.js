aa5b073175d89402535ee9f0d27e5e1c27d895e3
Jacob Marthaller <marthaller.jr@gmail.com>
2021-05-18 10:13:52 -0700

    finished sketch tutorial

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ function setup() {
 }
 
 function draw() {
-    background(200);
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255)
+    }
+    ellipse(mouseX, mouseY, 80, 80);
 }
 
-function draw() {
-    line(1, 70, 1000, 150)
-  }
+
