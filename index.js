09d126f8319d358d9419efcd896f26d801f671c3
Jacob Marthaller <marthaller.jr@gmail.com>
2021-05-31 12:59:49 -0700

    Draw lines

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ function draw() {
   rect(0, height*15/23, width * 2/19, height);
   fill(23, 13, 92);
   rect(width * 5/11, height*15/23, width * 10/13-width * 5/11, height * 22/23-height*15/23);
+
+  stroke(0);
+  strokeWeight(18);
+  line(0, height*10/24, width, height*10/24);
+  line(0, height*15/23, width, height*15/23);
+  stroke(14);
+  line(width * 5/11, height*10/24, width * 5/11, height*15/23);
+  strokeWeight(12);
+  line(width * 5/11 + 2, 0, width * 5/11+ 2, height*10/24);
+  strokeWeight(14);
+  line(width * 5/11+1, height*15/23, width * 5/11+1, height);
+  strokeWeight(12);
+  line(width * 2/19, height*15/23, width * 2/19, height);
+  strokeWeight(13);
+  line(width * 10/13, height*15/23, width * 10/13, height);
+  strokeWeight(14);
+  line(width * 5/11+1, height * 22/23, width * 10/13-1, height * 22/23);
 }
   
   
