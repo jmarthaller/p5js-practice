5e518eeb42ff52798d36aed65e7b438640293278
Jacob Marthaller <marthaller.jr@gmail.com>
2021-05-31 12:44:36 -0700

    Create base colors

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ function setup() {
 
 function draw() {
   background(253);
+
+  noStroke();
+  fill(231, 6, 4);
+  rect(0, 0, width * 5/11, height*10/24);
+  fill(253, 213, 4);
+  rect(0, height*15/23, width * 2/19, height);
+  fill(23, 13, 92);
+  rect(width * 5/11, height*15/23, width * 10/13-width * 5/11, height * 22/23-height*15/23);
 }
   
   
