7d37880d2d2e3fc9a77273784e3a96e2d54b72e3
Jacob Marthaller <marthaller.jr@gmail.com>
2021-05-31 09:38:12 -0700

    Add experimental line width

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function setup() {
     rect(0, 0, width * 5/11, height*10/24);
     fill(253, 213, 4);
     rect(0, height*15/23, width * 2/19, height);
-    fill(23, 13, 92);
+    fill(23, 13, 94);
     rect(width * 5/11, height*15/23, width * 10/13-width * 5/11, height * 22/23-height*15/23);
     
     stroke(0);
